ba4b4aa44e862d34bb88ef9ac6a4614649c80bda
Peter Imonte <imontepez@gmail.com>
2026-02-06 18:49:42 +0100

    Update BioViz-simple_code.R

--- BioViz-simple_code.R
+++ BioViz-simple_code.R
@@ -1,19 +1,8 @@
-my_name <- "Peter Imonte"
-
-affliation1 <- "University of Port Harcourt"
-
-affliation2 <- "Federal Medical Centre Umuahia"
-
-preffered_gene <- "PCSK9"
-
-gene_orgm <- "humans"
-
-reason1 <- "It represents the perfect intersection of clinical diagnostics, human genetic validation, and therapeutic innovation"
-
-reason2 <- "PCSK9 turned a complex, polygenic trait (heart disease) into something manageable. We can now use genetic screening to identify hyper-responders or high-risk individuals early in life."
-
+name <- "Peter Imonte"
+affiliation <- "University of Port Harcourt"
+favourite_gene <- "PCSK9"
+organism <- "humans"
 # final output ----
 
-my_essay <- paste("Hi, my name is", my_name , "a graduate of Biomedical Science from", affliation1, "currently interning as laboratory scientist at", affliation2, "As a laboratory scientist, my most preffered gene is", preffered_gene, "found in", gene_orgm, "I preffer",  preffered_gene,  "becacuse", reason1,  "and", reason2 )
-
-my_essay
+mywork <- paste0("Hi, my name is", name, "a graduate of Biomedical Science from", affiliation, "currently interning as laboratory scientist at", "As a laboratory scientist, my most preferred gene is", favourite_gene, "found in", organism)
+print(mywork)
