25c69b1d493808e75903032977c9e9774a5e17b4
Peter Imonte <imontepez@gmail.com>
2026-02-06 19:00:38 +0100

    Update BioViz-simple_code.R

--- BioViz-simple_code.R
+++ BioViz-simple_code.R
@@ -4,5 +4,5 @@ favourite_gene <- "PCSK9"
 organism <- "humans"
 # final output ----
 
-mywork <- paste0("Hi, my name is", name, "a graduate of Biomedical Science from", affiliation, "currently interning as laboratory scientist at", "As a laboratory scientist, my most preferred gene is", favourite_gene, "found in", organism)
+mywork <- paste0("Hi, my name is", name, "a graduate of Biomedical Science from", affiliation, "currently interning as laboratory scientist at Federal Medical Hospital, Umuahia", "As a laboratory scientist, my most preferred gene is", favourite_gene, "found in", organism)
 print(mywork)
